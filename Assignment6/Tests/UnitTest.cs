d534f95bff4be9d0cc0be81c2498099008fc5790
Aleeza Munir <aleeza.munir@riverty.com>
2024-02-29 15:57:54 +0100

    Test

---
+++ Assignment6/Tests/UnitTest.cs
@@ -0,0 +1,37 @@
+﻿using Assignment6.DTO.Characters;
+using Assignment6.Models;
+using Assignment6.Services.Characters;
+using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment6.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private readonly CharacterService characterService;
+        private readonly IMapper _mapper;
+        public UnitTest(CharacterService characterService, IMapper mapper) 
+        {
+            this.characterService = characterService;
+            this._mapper = mapper;
+        }
+        [TestMethod]
+        public void CreateCharacter()
+        {
+            //Arrange
+            var newCharacter = new CharacterCreateDTO()
+            {
+                Alias = "",
+                FullName = "",
+                Gender = "",
+                Picture = ""
+            };
+            //Act
+            var character = _mapper.Map<Character>(newCharacter);
+            var check = characterService.AddAsync(character);
+            //Assert
+            Assert.IsNotNull(check);
+        }
+    }
+}
